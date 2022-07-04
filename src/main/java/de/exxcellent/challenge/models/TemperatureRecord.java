d999384e3d47bc11bf29d8d85d2e36c8846a71b5
Andreas Krauss <am.krauss@web.de>
2022-07-04 10:05:42 +0200

    feat(weather): add model

---
+++ src/main/java/de/exxcellent/challenge/models/TemperatureRecord.java
@@ -0,0 +1,45 @@
+package de.exxcellent.challenge.models;
+
+public class TemperatureRecord {
+    private int id;
+    private int minTemperature;
+    private int maxTemperature;
+
+    private static int lowerTemperatureLimit = -148;
+    private static int upperTemperatureLimit =  153;
+
+    public TemperatureRecord(int id, int minTemperature, int maxTemperature) {
+        if (!validateTemperatureLimits(minTemperature, maxTemperature))
+            throw new IllegalArgumentException("Temperature exceeds limits");
+        if (!validateTemperatureValues(minTemperature, maxTemperature))
+            throw new IllegalArgumentException("maxTemperature lower than minTemperature");
+        this.id = id;
+        this.minTemperature = minTemperature;
+        this.maxTemperature = maxTemperature;
+    }
+
+    private boolean validateTemperatureLimits(int minTemperature, int maxTemperature){
+        if (minTemperature < lowerTemperatureLimit){ return false; }
+        if (minTemperature > upperTemperatureLimit){ return false; }
+        if (maxTemperature < lowerTemperatureLimit){ return false; }
+        if (maxTemperature > upperTemperatureLimit){ return false; }
+        return true;
+    }
+
+    private boolean validateTemperatureValues(int minTemperature, int maxTemperature){
+        if (maxTemperature < minTemperature){ return false; }
+        return true;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getMinTemperature() {
+        return this.minTemperature;
+    }
+
+    public int getMaxTemperature() {
+        return this.maxTemperature;
+    }
+}
