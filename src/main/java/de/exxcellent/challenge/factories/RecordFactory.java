24796e61da7ddbd1fec8ff9f53a84b8747179166
Andreas Krauss <am.krauss@web.de>
2022-07-04 14:27:36 +0200

    refactor(football): adapt factory using overloading

--- src/main/java/de/exxcellent/challenge/factories/RecordFactory.java
+++ src/main/java/de/exxcellent/challenge/factories/RecordFactory.java
@@ -1,12 +1,16 @@
 package de.exxcellent.challenge.factories;
 
 import de.exxcellent.challenge.interfaces.Record;
+import de.exxcellent.challenge.models.FootballRecord;
 import de.exxcellent.challenge.models.TemperatureRecord;
 
 public class RecordFactory {
 
     public Record createRecord(int id, int minTemperature, int maxTemperature){
         return new TemperatureRecord(id, minTemperature, maxTemperature);
+    }
+    public Record createRecord(String id, int minTemperature, int maxTemperature){
+        return new FootballRecord(id, minTemperature, maxTemperature);
     }
 
 }
