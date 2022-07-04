ab99668b2587edd03894fc698b794b0da2f33b46
Andreas Krauss <am.krauss@web.de>
2022-07-04 09:37:22 +0200

    feat(weather): add model tests

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Example JUnit 5 test case.
@@ -19,13 +20,104 @@ class AppTest {
     }
 
     @Test
-    void aPointlessTest() {
-        assertEquals("successful", successLabel, "My expectations were not met");
+    void createTemperatureRecord_validTemperature_validObject(){
+        int id = 1;
+        int minTemperature = 60;
+        int maxTemperature = 80;
+        TemperatureRecord record = new TemperatureRecore(id, minTemperature, maxTemperature);
+        assertEquals(id, record.getId());
+        assertEquals(minTemperature, record.getMinTemperature());
+        assertEquals(maxTemperature, record.getMaxTemperature());
     }
 
     @Test
-    void runFootball() {
-        App.main("--football", "football.csv");
+    void createTemperatureRecord_invalidLowMinTemperature_raiseException(){
+        int id = 1;
+        int minTemperature = -1200;
+        int maxTemperature = 80;
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            TemperatureRecord record = new TemperatureRecore(id, minTemperature, maxTemperature);;
+        });
+
+        String expectedMessage = "minTemperature too low";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
+
+    @Test
+    void createTemperatureRecord_invalidLowMaxTemperature_raiseException(){
+        int id = 1;
+        int minTemperature = 70;
+        int maxTemperature = -1200;
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            TemperatureRecord record = new TemperatureRecore(id, minTemperature, maxTemperature);;
+        });
+
+        String expectedMessage = "maxTemperature too low";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void createTemperatureRecord_invalidHighMinTemperature_raiseException(){
+        int id = 1;
+        int minTemperature = 1200;
+        int maxTemperature = 80;
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            TemperatureRecord record = new TemperatureRecore(id, minTemperature, maxTemperature);;
+        });
+
+        String expectedMessage = "minTemperature too low";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void createTemperatureRecord_invalidHighMaxTemperature_raiseException(){
+        int id = 1;
+        int minTemperature = 70;
+        int maxTemperature = 1200;
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            TemperatureRecord record = new TemperatureRecore(id, minTemperature, maxTemperature);;
+        });
+
+        String expectedMessage = "maxTemperature too low";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void createTemperatureRecord_MinTemperatureLowerThanMaxTemperature_raiseException(){
+        int id = 1;
+        int minTemperature = 90;
+        int maxTemperature = 70;
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            TemperatureRecord record = new TemperatureRecore(id, minTemperature, maxTemperature);;
+        });
+
+        String expectedMessage = "minTemperature lower than maxTemperature";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+//    @Test
+//    void aPointlessTest() {
+//        assertEquals("successful", successLabel, "My expectations were not met");
+//    }
+
+//    @Test
+//    void runFootball() {
+//        App.main("--football", "football.csv");
+//    }
 
 }
